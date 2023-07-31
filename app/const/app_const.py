ba092a13bea7fdf34993005060e6af2b291f054e
shogunskinoski <whitebirdsdk@gmail.com>
2023-07-31 16:27:54 +0300

    created app const class

---
+++ app/const/app_const.py
@@ -0,0 +1,9 @@
+class AppConst:
+    UI_PATH = 'resources/aivisiontech_gu_v2.ui'
+    BASE_C_PATH = 'c:\\'
+    EDGES = [
+            [6, 4, 2, 16, 0, 19, 1, 17, 3, 5, 7],
+            [1, 11, 9],
+            [0, 10, 8],
+            [12, 14, 18, 15, 13]
+        ]
