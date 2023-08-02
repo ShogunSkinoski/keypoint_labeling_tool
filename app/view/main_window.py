54d75e144bac7c4dece6a04ae08d0addb8ff1dc0
shogunskinoski <whitebirdsdk@gmail.com>
2023-08-02 15:06:34 +0300

    added raise error method

--- app/view/main_window.py
+++ app/view/main_window.py
@@ -3,7 +3,7 @@ from app.const.app_const import KeypointConst
 import numpy as np
 import cv2
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 #TODO: create fields for the constant ui values like thickness, color, etc.
 
 class MainWindow(QMainWindow):
@@ -17,17 +17,19 @@ class MainWindow(QMainWindow):
     #TODO: refactor this method
     def draw_skeloton(self, positions):        
         self.img = self.base_img.copy()
-        for i in KeypointConst.EDGES:
+        for id_list in KeypointConst.EDGES:
             poly_lines = []
-            for j in i:
-                poly_lines.append(positions[j][:-1])
+            for keypoint_id in id_list:
+                poly_lines.append(positions[keypoint_id][:-1])
             draw_poly = np.array(poly_lines)
+            
             cv2.polylines(self.img, [draw_poly], isClosed=False,
                           color=(0,255,0), thickness=2, lineType=cv2.LINE_4)
-        for count1, pose in enumerate(positions):
-            cv2.circle(self.img, center=(pose[0], pose[1]), radius=4, color=(
+            
+        for index, pose_coord in enumerate(positions):
+            cv2.circle(self.img, center=(pose_coord[0], pose_coord[1]), radius=4, color=(
                 (0,123,0)), thickness=-2, lineType=cv2.LINE_4)
-            self.draw_skeloton_text(positions, count1)
+            self.draw_skeloton_text(positions, index)
             
     #TODO: refactor this method
     def draw_skeloton_text(self, positions, index):
@@ -42,11 +44,22 @@ class MainWindow(QMainWindow):
                         fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.75, color=(100, 100, 255),
                         thickness=2,
                         lineType=cv2.LINE_4)    
+    
+    def raise_error_message(self, message):
+        error_box = QMessageBox()
+        error_box.setIcon(QMessageBox.Critical)
+        error_box.setText(message)
+        error_box.setWindowTitle("Error")
+        error_box.setStandardButtons(QMessageBox.Ok)
+        return_value = error_box.exec_()
+        if return_value == QMessageBox.Ok:
+            return
+        
         
     def start_labeling(self, mouse_callback):
         cv2.namedWindow('labeling window', cv2.WINDOW_AUTOSIZE),
         cv2.setMouseCallback("labeling window", mouse_callback)
-        while (1):
+        while (True):
             cv2.imshow('labeling window', self.img)
             key = cv2.waitKey(1) & 0xFF
             if key == ord("s"):
